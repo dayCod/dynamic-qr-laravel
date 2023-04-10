7b3360a54837c1d24588843144650ec82ceb5516
dayCod <alayarahman07@gmail.com>
2023-04-10 22:56:58 +0700

    update: employee function

--- app/Http/Controllers/Dashboard/Employee/EmployeeController.php
+++ app/Http/Controllers/Dashboard/Employee/EmployeeController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers\Dashboard\Employee;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
+    private function rules()
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email'],
+            'department_id' => ['required', 'exists:departments,id'],
+        ];
+    }
+
     public function index()
     {
         return view('dashboard.employee.index', [
@@ -23,11 +33,23 @@ class EmployeeController extends Controller
 
     public function createEmployee(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'email'],
-            'department_id' => ['required', 'exists:departments,id'],
+        $request->validate($this->rules());
+
+        $employee = Employee::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'department_id' => $request->department_id,
         ]);
+
+        $result = (object) [
+            'success' => true,
+            'message' => 'Employee Berhasil Dibuat',
+            'employee_id' => $employee->id,
+        ];
+
+        return redirect()
+            ->route('dashboard.employee.index')
+            ->with('success', $result->message);
     }
 
     public function edit($id)
@@ -35,5 +57,40 @@ class EmployeeController extends Controller
         return view('dashboard.employee.edit', [
             'title' => 'Edit Employee',
         ]);
+    }
+
+    public function updateEmployee(Request $request, $id)
+    {
+        $request->validate($this->rules());
+
+        $employee = Employee::find($id)->fill([
+            'name' => $request->name,
+            'email' => $request->email,
+            'department_id' => $request->department_id,
+        ])->save();
+
+        $result = (object) [
+            'success' => true,
+            'message' => 'Employee Berhasil Diupdate',
+            'employee_id' => $id,
+        ];
+
+        return redirect()
+            ->route('dashboard.employee.index')
+            ->with('success', $result->message);
+    }
+
+    public function deleteEmployee($id)
+    {
+        $employee = Employee::find($id);
+        $employee->delete();
+
+        $result = (object) [
+            'success' => true,
+            'message' => 'Employee Berhasil Dihapus',
+            'employee_id' => $id,
+        ];
+
+        return response()->json(['sucess' => $result->message], 200);
     }
 }
